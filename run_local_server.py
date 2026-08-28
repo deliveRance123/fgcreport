f4de8851462844d20d8b5dc6687f9a0899207b41
Deliverance <deliverancetech200@gmail.com>
2026-08-28 05:06:29 +0100

    Add run_local_server.py launcher script

---
+++ run_local_server.py
@@ -0,0 +1,7 @@
+import uvicorn
+
+if __name__ == "__main__":
+    print("=======================================================")
+    print("Starting Foursquare Report System on http://127.0.0.1:8000")
+    print("=======================================================")
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
